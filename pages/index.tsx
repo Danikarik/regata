57f1810f26cea075673d2f2350075089533c5018
danikarik <daniyar@hey.com>
2021-01-03 01:44:38 +0600

    Add Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from "next/head"
+import Hero from "../components/Hero"
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Hero />
 
       <main>
         <h1>Hello World</h1>
@@ -18,3 +21,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
